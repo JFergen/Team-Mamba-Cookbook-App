0e0213c082e0addff2040ff041fe5328ffb9b533
nick Ghuman <NishcheyjitGhuman@my.unt.edu>
2021-03-09 13:47:59 -0600

    Revert "Revert "intial cards""
    This reverts commit 3cd00bb32edbeb1aa826cdbb3a483c6007bb6782.

--- cookbook-app/src/pages/Home/Home.js
+++ cookbook-app/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DatabaseDriver from '../../database/DatabaseDriver';
 import ReactList from 'react-list';
 import './Home.css';
+import Card from '../../Cards'
 
 class Home extends Component {
     constructor() {
@@ -47,6 +48,20 @@ class Home extends Component {
                         type='uniform'
                     />
                 </div>
+            </div>
+        )
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <Card
+                title='Card Title'
+                imageURL='https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F4f%2F82%2F8d%2F4f828d05f82b8b7aedfe8be6a7d9d2a3.png&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F350366046007007479%2F&tbnid=-3dOdBkeOueX-M&vet=12ahUKEwjm8rqXsqHvAhW6gU4HHTwGCCsQMygBegUIARCoAQ..i&docid=exUuJz2767CCOM&w=2500&h=2539&q=coding%20meme&ved=2ahUKEwjm8rqXsqHvAhW6gU4HHTwGCCsQMygBegUIARCoAQ'
+                body='This is our reciepe'
+                ingredient='everything available'
+                time='0 mins'
+                 />
             </div>
         )
     }
