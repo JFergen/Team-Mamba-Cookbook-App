bcb5af6d5ea294d7378abadc895f4833634aaaef
nick Ghuman <NishcheyjitGhuman@my.unt.edu>
2021-03-14 23:12:59 -0500

    card final

--- cookbook-app/src/pages/Home/Home.js
+++ cookbook-app/src/pages/Home/Home.js
@@ -5,6 +5,8 @@ import './Home.css';
 import { Card } from "react-bootstrap";
 import {MdSave, MdChatBubbleOutline} from 'react-icons/md'
 import { IconContext } from 'react-icons';
+import FormComponent from '../../components/star-rating';
+
 class Home extends Component {
     constructor() {
         super();
@@ -58,6 +60,7 @@ class Home extends Component {
             <Card className="m-3">
                 <Card.Header className="p-0 text-center bg-secondary">
                         <h class="text-dark font-weight-bold">Pasta Food Stuff</h>
+                        <h className="preview_usr_stars"> <FormComponent/></h>
                         <a href="#" class="float-right" style={{color: 'black'}}><MdSave/></a>
                 </Card.Header>
                     <Card.Body class="table-warning">
@@ -65,33 +68,26 @@ class Home extends Component {
                         <Card.Text className="mt-2 text-dark">
                             This reciepe is all about being super good, super yummy and wow it is so easy to cook its so crazy. Please take a look at it and you will love it I promise. I have made it my mission in life to bless those with this recipe passed on through my family. You are welcome.  
                         </Card.Text>
-                        <div class="d-flex justify-content-around">
-                            <div class='one'>
-                                <Card.Text className="text-dark">
-                                    Ingredients
-                                </Card.Text>
+                        <div class="d-flex justify-content-around text-dark">
+                            <div class='one'> Ingredients 
                                 <div class="box">
                                     test
                                 </div>
                             </div>
-                            <div class='two'>
-                                <Card.Text className="text-dark">
-                                    Info
-                                </Card.Text>
+                            <div class='two'> Info
                                 <div class="box">
                                     test
                                 </div>
                             </div>
-                            <div class='three'>
-                                <Card.Text className="text-dark">
-                                    Directions
-                                </Card.Text>
+                            <div class='three'> Directions
                                 <div class="box">
                                     test
                                 </div>
                             </div>
                         </div>
-                        <a href="#" class="nav-link text-primary float-left"><MdChatBubbleOutline/>Comments</a>
+                        <p></p>
+                        <a href="#" class="nav-link text-primary float-left"><MdChatBubbleOutline/>Comments</a> 
+                        <a href="#" style={{paddingLeft: 1300, color: 'blue'}} >#Yummy</a>
                         <a href="#" class="btn btn-primary float-right float-bottom" id="right-panel-link">Recipe Page</a>
                     </Card.Body>
                 <Card.Footer class="p-0 bg-secondary">
