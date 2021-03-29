2775308401cb29c810dcc8ec4e54f2c05450d9d7
JFergen <josephfergen150@gmail.com>
2021-03-29 12:32:07 -0500

    Merged Nick's create page

--- cookbook-app/src/components/card.js
+++ cookbook-app/src/components/card.js
@@ -20,28 +20,35 @@ class CardComponent extends Component {
                     <a href="#" class="float-right" style={{color: 'black'}}><MdSave size={32}/></a>
             </Card.Header>
                 <Card.Body class="table-warning">
-                    <Card.Img class="card-img-left" src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg"/>
+                    <Card.Img
+                        class="card-img-left"
+                        height='700px'
+                        src={this.props.recipe.image}
+                    />
                     <Card.Text className="mt-2 text-dark">{this.props.recipe.description}</Card.Text>
                     <div class="d-flex justify-content-around text-dark">
                         <div class='one'>Ingredients 
                             <div class="box">
-                                test
+                                {this.props.recipe.ingredients}
                             </div>
                         </div>
                         <div class='two'>Info
                             <div class="box">
-                                test
+                                {this.props.recipe.time}
                             </div>
                         </div>
                         <div class='three'>Directions
                             <div class="box">
-                                test
+                                {this.props.recipe.directions}
                             </div>
                         </div>
                     </div>
                     <a href="#" class="nav-link text-primary float-left"><MdChatBubbleOutline/>Comments</a> 
                     <a href="#" class="btn btn-primary btn-sm float-right p-2" id="right-panel-link">Recipe Page</a>
-                    <a href="#" class= "float-right p-1" style={{ color: 'blue'}}>#Yummy</a>
+                    {/* {this.props.recipe.tags.map((value, index) => {
+                        return (<a href="#" key={index} class= "float-right p-1" style={{ color: 'blue'}}>#{this.props.recipe.tags[index]}</a>)
+                    })} */}
+                    {/* <a href="#" class= "float-right p-1" style={{ color: 'blue'}}>{this.props.recipe.tags}</a> */}
                 </Card.Body>
             <Card.Footer class="p-0 bg-secondary">
                 <h class="text-light font-weight-bold">Made by:</h>
