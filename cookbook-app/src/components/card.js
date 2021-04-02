b5a158766b7e62d383fddbf4312c44dbacddb994
Joseph Fergen <josephfergen150@gmail.com>
2021-04-01 21:19:41 -0500

    Fixed function name

--- cookbook-app/src/components/card.js
+++ cookbook-app/src/components/card.js
@@ -20,17 +20,17 @@ class CardComponent extends Component {
     }
 
     async componentDidMount() {
-        let savedRecipeIds = await DatabaseDriver.getSavedArray(this.props.user.googleId)
-        console.log(savedRecipeIds.saved_recipes[0])
+        let savedRecipeIds = await DatabaseDriver.getUsersSavedRecipes(this.props.user.googleId)
+        console.log(savedRecipeIds)
         console.log(this.props.recipe._id.$oid)
 
-        for (let i = 0; i < savedRecipeIds.length; i++) {
-            if (savedRecipeIds.saved_recipes[i] === this.props.recipe._id.$oid) {
-                console.log('got here')
-                this.setState({ saved: true })
-                break
-            }
-        }
+        // for (let i = 0; i < savedRecipeIds.length; i++) {
+        //     if (savedRecipeIds.saved_recipes[i] === this.props.recipe._id.$oid) {
+        //         console.log('got here')
+        //         this.setState({ saved: true })
+        //         break
+        //     }
+        // }
         
     }
 
