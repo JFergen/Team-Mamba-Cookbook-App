0c3c6cec9bb0f8a8cc62d202432675053809f214
DrChie <imdatchie@gmail.com>
2021-04-08 12:01:09 -0500

    Error Solving

--- cookbook-app/src/pages/Saved/Saved.js
+++ cookbook-app/src/pages/Saved/Saved.js
@@ -27,7 +27,8 @@ class Saved extends Component {
 
     async getRecipes() {
         const data = await DatabaseDriver.getUserSaved(this.state.user.googleId);    // Gets recipes from a user
-        this.setState({ recipes: data })
+        const dataPostJson = JSON.parse(data) 
+        this.setState({ recipes: dataPostJson })
     }
 
     renderItem(index, key) {
