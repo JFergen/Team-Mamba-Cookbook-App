c72fe185a106ea535ae365987f9a531b3da63de4
Joseph Fergen <josephfergen150@gmail.com>
2021-04-07 22:15:44 -0500

    Minor card layout changed

--- cookbook-app/src/components/card.js
+++ cookbook-app/src/components/card.js
@@ -50,8 +50,6 @@ class CardComponent extends Component {
                 }
             })
         }
-
-        
     }
 
     saveRecipe() {
@@ -116,7 +114,7 @@ class CardComponent extends Component {
                             </div>
                             <div class='two font-weight-bold'>Info
                                 <div class="box font-weight-normal">
-                                    {this.props.recipe.time}
+                                    Time Taken: {this.props.recipe.time}
                                 </div>
                             </div>
                             <div class='three font-weight-bold'>Directions
