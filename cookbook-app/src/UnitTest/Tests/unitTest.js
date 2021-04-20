2baea08b557a12099781a4f57515dbff33fded05
Hetpatel1234 <61066155+Hetpatel1234@users.noreply.github.com>
2021-04-20 03:36:24 -0500

    Added unit testing for back-end functions
    Added unit testing for back-end functions

---
+++ cookbook-app/src/UnitTest/Tests/unitTest.js
@@ -0,0 +1,17 @@
+import DatabaseDriver from '../../database/DatabaseDriver';
+
+it("Should get N random recipes", async () => {
+    async function getNRandomRecipes(doc) {
+        let isPass;
+        await DatabaseDriver.getNRandomRecipes("102050791948382455788", 10)
+        .then(() => {
+            console.log("Successfully got random recipes.");
+            isPass = true;
+        })
+        .catch((error) => {
+            alert("Error getting random recipes.", error);
+            isPass = false;
+        });
+        return isPass;
+    }
+});
